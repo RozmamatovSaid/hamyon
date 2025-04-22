ffc04508eaf8c7d1835c590ac1d4e94279c7cf84
recipeapp <caidcode@gmail.com>
2025-04-22 04:16:31 -0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeScreen());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
-//
